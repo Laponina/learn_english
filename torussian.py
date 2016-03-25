75886670ecc53609a82e9243fbf96c9138d60341
laponina <laponinalubagmail.com>
2016-03-25 18:56:26 +0700

    разобраться с TODO: сделать иправление строки, разобраться, почему заходит дважды в функции

--- torussian.py
+++ torussian.py
@@ -6,11 +6,13 @@ import time
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.QtGui import QColor
 from PyQt5.uic import loadUi
+from PyQt5.QtCore import Qt
 
 # KOL_WORDS = 2  # временно, заменить потом на 10
 
 location = lambda x: os.path.join(
     os.path.dirname(os.path.realpath(__file__)), x)
+
 
 class Translate_RU(QDialog):
     def __init__(self):
@@ -37,17 +39,18 @@ class Translate_RU(QDialog):
         self.buttonANSWER.autoDefault()
         self.buttonANSWER.clicked.connect(self.button_answer)
         self.buttonNEXT.clicked.connect(self.button_next)
-        # self.progress.minimum =
         self.progress.setRange(1, len(self.words))
         self.update_progress()
-
 
     def update_progress(self):
         self.progress.setValue(self.num_done + 1)
         self.partProcess.setText('%s / %s' % (str(self.num_done + 1), str(len(self.words))))
 
-
     def button_answer(self):
+        # TODO: заходит лишний раз, думаю сделать зависимость от названия, но почему-то не выходит
+        print('button ANSWER clicked')
+        # if self.buttonANSWER.text == 'ANSWER':
+        #     print('button ANSWER clicked!!')
         self.russian_word.setStyleSheet("QTextEdit { color: %s }" % self.col.name())
         if self.russian_word.toPlainText() == self.words[self.num_done]['russian_word']:  # если слово введено правильно
             print('YES')
@@ -62,12 +65,10 @@ class Translate_RU(QDialog):
             self.russian_word.setStyleSheet(
                 "QTextEdit { color: %s }" % self.col_false.name())  # меняю цвет текста на красный
         self.buttonANSWER.setText('Next')  # меняю название кнопки
-        # widget.english_word.clear()
-        # widget.russian_word.clear()
         self.buttonANSWER.clicked.connect(self.answ_next)
 
-
     def button_next(self):
+        print('button DONT KNOW clicked')
         self.error.setText(self.words[self.num_done]['russian_word'])  # выводим правильный ответ
         self.russian_word.setStyleSheet(
             "QTextEdit { color: %s }" % self.col_false.name())  # меняю цвет текста на красный
@@ -75,8 +76,8 @@ class Translate_RU(QDialog):
         self.buttonANSWER.setText('Next')  # меняю название кнопки
         self.buttonANSWER.clicked.connect(self.answ_next)
 
-
     def answ_next(self):
+        print('button NEXT clicked')
         self.num_done += 1
         self.russian_word.setStyleSheet("QTextEdit { color: %s }" % self.col.name())  # возвращаю черный цвет текста
         self.buttonNEXT.setDisabled(False)  # делаем кнопку активной
@@ -90,30 +91,30 @@ class Translate_RU(QDialog):
             self.english_word.setText(self.words[self.num_done]['english_word'])  # append
             self.remark.setText(self.words[self.num_done]['remark'])
         else:
-            pass
+            self.showChildWindow()
         self.update_progress()
         # self.progress.value = self.num_done
 
-class results(QDialog):
+    def showChildWindow(self):
+        self.childForm, self.childWindow = Results.init(self.mainwindow)
+        self.childWindow.show()
+
+
+class Results(QDialog):
     def __init__(self):
-        super(SettingsWidgets, self).__init__()
+        super().__init__()
 
+        loadUi('qt_ui/results.ui', self)
+        self.setWindowModality(Qt.WindowModal)
+        self.setupUi()
 
+        def setupUi(self):
+            self.percentage.setText('сделано верно %s из %s' % (str(widget.num_true), str(widget.num_done)))
 
 
 def line_treatment(ru_str):
     # TODO: убрать лишние пробелы из введенной строки (потом)
     pass
-
-
-# def make_words(words):
-#     когда будет словарь. Собирает список слов для тренировки
-#     while len(words) < 10:
-#         el = random.randint(0, len(data))
-#         if el not in words:
-#             words.append(el)
-#
-# words = []
 
 
 if __name__ == '__main__':
