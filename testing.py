dac803cc8d1d37cb033d9ecbc5dd0f192b63b687
laponina <laponinalubagmail.com>
2016-05-13 19:48:59 +0700

    Поправила некоторые мелочи, сделала тестирование. Осталось сделать установшик, рейтинг (скорей всего, не успею).

---
+++ testing.py
@@ -0,0 +1,10 @@
+dict = {
+        "sos": 1,
+        "mam": 2
+        }
+n = 3
+if n % 2 == 0:
+    k = "sos"
+else:
+    k = "mam"
+print(dict[k])
--- torussian.py
+++ torussian.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import json
-from PyQt5.QtWidgets import QApplication, QDialog, QTableWidgetItem, QWidget, QMessageBox
+from PyQt5.QtWidgets import QApplication, QDialog, QTableWidgetItem, QComboBox
 from PyQt5.QtGui import QColor, QStandardItem
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import Qt
@@ -10,22 +10,53 @@ location = lambda x: os.path.join(
     os.path.dirname(os.path.realpath(__file__)), x)
 
 
-class Translate_RU(QDialog):
+class Choose_themeRU(QDialog):
     def __init__(self, parent):
         super().__init__()
-        # TODO: вопрос про уровень/тему (в рус-англ тоже) запускается дочернеее
-        # loadUi('qt_ui/question.ui', self)
+        loadUi('qt_ui/question.ui', self)
+        self.parent = parent
+        self.words = self.parent.words
+        self.themes = self.parent.themes
+        self.selectedtheme = None
+        self.initui()
+        self.show()
+
+    def initui(self):
+        for theme in self.themes:
+            self.box_themes.addItem(theme)
+        self.selectedtheme = self.box_themes.currentText()
+        print(self.selectedtheme)
+        self.start_test.clicked.connect(self.complete)
+
+    def complete(self):
+        print("complete")
+        self.to_russian = Translate_RU(self, self.selectedtheme)
+        self.close()
+
+
+class Translate_RU(QDialog):
+    def __init__(self, parent, theme):
+        super().__init__()
         loadUi('qt_ui/torussian.ui', self)
         self.parent = parent
+        self.theme = theme
         self.col_false = QColor(255, 0, 0)
         self.col_true = QColor(0, 255, 0)
         self.col = QColor(0, 0, 0)
         self.num_done = 0
         self.num_true = 0
         self.wrong_words = []
-        self.words = self.parent.words
+        self.themes = self.parent.themes
+        self.words = []
+        self.all_words = self.parent.words
+        self.addwords()
         self.initui()
         self.show()
+
+    def addwords(self):
+        for word in self.all_words:
+            if word["theme"] == self.theme:
+                self.words.append(word)
 
     def initui(self):
         self.english_word.setText(self.words[self.num_done]['english_word'])  # append
@@ -82,7 +113,7 @@ class Translate_RU(QDialog):
 
     def complete(self):
         print("complete")
-        self.results = Results(self)
+        self.results = Results(self)  # вывод результатов - новое окно results
         self.close()
 
     def closeEvent(self, e):
@@ -90,21 +121,54 @@ class Translate_RU(QDialog):
         self.close()
 
 
-class Translate_EN(QDialog):
+class Choose_themeEN(QDialog):
     def __init__(self, parent):
+        super().__init__()
+        loadUi('qt_ui/question.ui', self)
+        self.parent = parent
+        self.words = self.parent.words
+        self.themes = self.parent.themes
+        self.selectedtheme = None
+        self.initui()
+        self.show()
+
+    def initui(self):
+        for theme in self.themes:
+            self.box_themes.addItem(theme)
+        self.selectedtheme = self.box_themes.currentText()
+        print(self.selectedtheme)
+        self.start_test.clicked.connect(self.complete)
+
+    def complete(self):
+        print("complete")
+        self.to_english = Translate_EN(self, self.selectedtheme)
+        self.close()
+
+
+class Translate_EN(QDialog):
+    def __init__(self, parent, theme):
         super().__init__()
 
         loadUi('qt_ui/toenglish.ui', self)
         self.parent = parent
+        self.theme = theme
+        self.themes = self.parent.themes
         self.col_false = QColor(255, 0, 0)
         self.col_true = QColor(0, 255, 0)
         self.col = QColor(0, 0, 0)
         self.num_done = 0
         self.num_true = 0
-        self.words = self.parent.words
+        self.all_words = self.parent.words
+        self.words = []
         self.wrong_words = []
+        self.addwords()
         self.initui()
         self.show()
+
+    def addwords(self):
+        for word in self.all_words:
+            if word["theme"] == self.theme:
+                self.words.append(word)
 
     def initui(self):
         self.russian_word.setText(self.words[self.num_done]['russian_word'])  # append
@@ -161,6 +225,7 @@ class Translate_EN(QDialog):
 
     def complete(self):
         print("complete")
+        # FIXME: почему-то окно с результатми под главным окном
         self.results = Results(self)
         self.close()
 
@@ -214,6 +279,7 @@ class MainWindow(QDialog):
         loadUi('qt_ui/main.ui', self)
         self.initui()
         self.words = None
+        self.themes = None
         self.load_data()
         self.show()
 
@@ -228,12 +294,15 @@ class MainWindow(QDialog):
         with open(location('data/elementary words')) as f:
             self.words = json.load(f)
 
+        with open(location('data/themes')) as f:
+            self.themes = json.load(f)
+
     def btn_russian(self):
-        self.to_russian = Translate_RU(self)
+        self.choose_theme = Choose_themeRU(self)
         self.close()
 
     def btn_english(self):
-        self.to_english = Translate_EN(self)
+        self.choose_theme = Choose_themeEN(self)
         self.close()
 
     def btn_customization(self):
@@ -250,8 +319,137 @@ class StartTest(QDialog):
         super().__init__()
 
         loadUi('qt_ui/test.ui', self)
+        self.parent = parent
+        self.all_words = self.parent.words
+        self.words = None
+        self.wrong_words = []
+        self.num_done = 0  # количество сделанных
+        self.num_true = 0  # количество сделанных правильно
+        self.col_false = QColor(255, 0, 0)
+        self.col_true = QColor(0, 255, 0)
+        self.col = QColor(0, 0, 0)
+        self.lan_remark = None  # примечание на нужном языке
+        self.lan_translate = None  # слово на нужном языке
+        self.lan_word = None  # слово , которое нужно перевести, на нужном языке
         self.show()
-        # TODO: скачать тест на определение уровня
+        self.choose_words()
+        self.initUi()
+
+    def choose_words(self):
+        self.words = self.all_words  # временно
+
+    def initUi(self):
+        if self.num_done % 2 == 0:
+            self.lan_translate = "english_word"
+            self.lan_word = "russian_word"
+            self.lan_remark = "rus_remark"
+        else:
+            self.lan_word = "english_word"
+            self.lan_language = "russian_word"
+            self.lan_remark = "remark"
+        self.word.setText(self.words[self.num_done]['russian_word'])  # append
+        self.remark.setText(self.words[self.num_done]['rus_remark'])
+        self.buttonANSWER.setText('ANSWER')
+        self.buttonANSWER.autoDefault()
+        self.buttonANSWER.clicked.connect(self.button_answer)
+        self.buttonSTOP.clicked.connect(self.button_stop)
+        self.progress.setRange(1, len(self.words))
+        self.update_progress()
+
+    def button_answer(self):
+        print('button ANSWER clicked')
+        if self.num_done % 2 == 0:
+            self.lan_translate = "english_word"
+            self.lan_word = "russian_word"
+            self.lan_remark = "rus_remark"
+        else:
+            self.lan_word = "english_word"
+            self.lan_translate = "russian_word"
+            self.lan_remark = "remark"
+        self.translated_word.setStyleSheet("QTextEdit { color: %s }" % self.col.name())
+
+        print(self.lan_translate)
+        print(self.lan_word)
+        if line_treatment(self.translated_word.toPlainText()) == self.words[self.num_done][self.lan_translate]:
+            # если слово введено правильно
+            print('YES')
+            self.translated_word.setStyleSheet(
+                "QTextEdit { color: %s }" % self.col_true.name())  # меняю цвет текста на зеленый
+            self.num_true += 1
+            self.words[self.num_done]["rating"] -= 10
+        else:
+            print('NO')
+            self.wrong_words.append(self.words[self.num_done])
+            self.error.setText(self.words[self.num_done][self.lan_translate])  # выводим правильный ответ
+            self.translated_word.setStyleSheet(
+                "QTextEdit { color: %s }" % self.col_false.name())  # меняю цвет текста на красный
+        self.buttonANSWER.setText('Next')  # меняю название кнопки
+        self.buttonANSWER.clicked.connect(self.answ_next)
+        self.buttonANSWER.clicked.disconnect(self.button_answer)
+
+    def answ_next(self):
+        print('button NEXT clicked')
+        self.num_done += 1
+        if self.num_done % 2 == 0:
+            self.lan_translate = "english_word"
+            self.lan_word = "russian_word"
+            self.lan_remark = "rus_remark"
+        else:
+            self.lan_word = "english_word"
+            self.lan_translate = "russian_word"
+            self.lan_remark = "remark"
+
+        self.translated_word.setStyleSheet("QTextEdit { color: %s }" % self.col.name())  # возвращаю черный цвет текста
+        self.buttonANSWER.setText('ANSWER')  # меняю название кнопки
+        self.buttonANSWER.clicked.connect(self.button_answer)
+        self.buttonANSWER.clicked.disconnect(self.answ_next)
+        if self.num_done < len(self.words):
+            self.translated_word.clear()
+            self.word.clear()
+            self.error.clear()
+            self.word.setText(self.words[self.num_done][self.lan_word])  # append
+            self.remark.setText(self.words[self.num_done][self.lan_remark])
+
+        self.update_progress()
+
+        if self.num_done >= len(self.words):
+            self.complete()
+
+    def button_stop(self):
+        # TODO: сделать воспрос: вы действительно хотите закночить тест преждевремено
+        self.question_ex = Exit_question(self)
+
+    def update_progress(self):
+        self.progress.setValue(self.num_done + 1)
+        self.partProcess.setText('%s / %s' % (str(self.num_done + 1), str(len(self.words))))
+
+    def complete(self):
+        self.results = Results(self)
+        self.close()
+
+    def closeEvent(self, e):
+        self.main = MainWindow()
+        self.close()
+
+
+class Exit_question(QDialog):
+    def __init__(self, parent):
+        super().__init__()
+        loadUi('qt_ui/exit_question.ui', self)
+        self._parent = parent
+        self.show()
+        self.initui()
+
+    def initui(self):
+        self.bttnYES.clicked.connect(self.bttn_yes)
+        self.bttnNO.clicked.connect(self.bttn_no)
+
+    def bttn_yes(self):
+        self.close()
+        self._parent.close()
+
+    def bttn_no(self):
+        self.close()
 
 
 class Customization(QDialog):
@@ -260,16 +458,17 @@ class Customization(QDialog):
         loadUi('qt_ui/dictionary.ui', self)
         self.parent = parent
         self.words = self.parent.words
-        self.column = 4
+        self.themes = self.parent.themes
+        self.column = 5
         self.row = len(self.words)
-        self.width = 492
+        self.width = 490
         self.hight = 270
         self.k_row = 0
         self.initui()
         self.show()
 
     def initui(self):
-        self.list_words.setColumnCount(4)
+        self.list_words.setColumnCount(5)
         self.list_words.setRowCount(len(self.words))
         self.list_words.verticalHeader().hide()
         self.list_words.horizontalHeader().hide()
@@ -283,6 +482,7 @@ class Customization(QDialog):
             self.list_words.setItem(self.k_row, 1, QTableWidgetItem(word['russian_word']))
             self.list_words.setItem(self.k_row, 2, QTableWidgetItem(word['remark']))
             self.list_words.setItem(self.k_row, 3, QTableWidgetItem(word['rus_remark']))
+            self.list_words.setItem(self.k_row, 4, QTableWidgetItem(word['theme']))
             self.k_row += 1
         self.add_words.clicked.connect(self.btn_add)
         self.toremove.clicked.connect(self.btn_remove)
@@ -291,7 +491,6 @@ class Customization(QDialog):
         self.toback.clicked.connect(self.close)
 
     def btn_remove(self):
-        # TODO: обработка ошибок
         index = self.list_words.row(self.list_words.currentItem())
         print(index)
         self.words.pop(index)
@@ -301,21 +500,21 @@ class Customization(QDialog):
         print('удалено')
 
     def btn_save(self):
-        # TODO: обработка ошибок
         self.words = []
-        print(self.list_words.rowCount())
         el = 0
-        while el < self.list_words.rowCount() - 1:
+        while el < self.list_words.rowCount():
             en_word = self.list_words.item(el, 0).text()
             ru_word = self.list_words.item(el, 1).text()
             remark = self.list_words.item(el, 2).text()
             ru_remark = self.list_words.item(el, 3).text()
+            theme = self.list_words.item(el, 4).text()
             new_word = {
                 "russian_word": ru_word,
                 "rus_remark": ru_remark,
                 "remark": remark,
                 "english_word": en_word,
-                "rating": 100
+                "rating": 100,
+                "theme": theme
             }
             el += 1
             self.words.append(new_word)
@@ -325,15 +524,24 @@ class Customization(QDialog):
         with open(location('data/elementary words'), 'w') as f:
             json.dump(self.words, f, ensure_ascii=False)
 
+    def load_data(self):
+        with open(location('data/elementary words')) as f:
+            self.words = json.load(f)
+
     def btn_cancel(self):
         self.load_data()
         self.list_words.clear()
         self.k_row = 0
+        self.load_data()
+        self.list_words.setRowCount(len(self.words))
+        # TO DO: сделать автоперенос последней строки
+
         for word in self.words:
             self.list_words.setItem(self.k_row, 0, QTableWidgetItem(word['english_word']))
             self.list_words.setItem(self.k_row, 1, QTableWidgetItem(word['russian_word']))
             self.list_words.setItem(self.k_row, 2, QTableWidgetItem(word['remark']))
             self.list_words.setItem(self.k_row, 3, QTableWidgetItem(word['rus_remark']))
+            self.list_words.setItem(self.k_row, 4, QTableWidgetItem(word['theme']))
             self.k_row += 1
 
     def load_data(self):
@@ -341,7 +549,6 @@ class Customization(QDialog):
             self.words = json.load(f)
 
     def btn_add(self):
-        # TODO: how add theme of new word
         self.list_words.setRowCount(self.list_words.rowCount() + 1)
 
     def closeEvent(self, e):
@@ -352,9 +559,6 @@ class Customization(QDialog):
 def line_treatment(str):
     str = ' '.join(str.split())
     return str
-
-
-# TODO: сделать паузу, сохранение индекса слова (в файл index pause?)
 
 
 if __name__ == '__main__':
